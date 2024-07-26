c57643b39f04400ac12ea25b1bf24f9bbf08c6d8
marwaywoodsian <83570907+marwaywoodsian@users.noreply.github.com>
2024-07-26 00:35:36 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,7 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+//const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "http://marwaywoodsian.com/home";
 
 ghpages.publish(
   pathname,
